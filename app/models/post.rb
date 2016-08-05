be840f6486194026f914f5223561198b9f354b18
= <yohann.jolain@reveauxlettres.fr>
2016-08-05 21:23:42 +0200

    Added comments related field to post, also report

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,9 @@ class Post
   field :score, type: Integer, default: 0
 
   belongs_to :user, class_name: "User", inverse_of: :posts
+
+  has_many :comments, class_name: "Comment", inverse_of: :post
+
+  field :report, type: Boolean, default: false
+  alias_method :report?, :report
 end
