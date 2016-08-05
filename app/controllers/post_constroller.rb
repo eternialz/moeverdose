a83b49fb85fc42b898f39267107e10cccff69f8b
Erfani <sebastien.erfani@reveauxlettres.fr>
2016-08-05 21:41:21 +0200

    Started posts controller

---
+++ app/controllers/post_constroller.rb
@@ -0,0 +1,46 @@
+class HomeController < ApplicationController
+
+  before_action :set_question, only: [:edit, :update, :show, :destroy]
+
+  def show
+
+  end
+
+  def index
+    @posts = Posts.all
+  end
+
+  def new
+    @post ||= Posts.new    
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+  def set_post
+    if params[:id]
+      @question = FrequentlyAskedQuestion::Root.find(params[:id])
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(
+      :name
+    )
+  end
+
+end
