24717176e3b1fd6318c87336f6e30604fbdd1f0f
Aruisehu <Yohann.JOLAIN@supinfo.com>
2016-08-10 08:45:18 +0200

    Installed MongoDB properly

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,16 @@
 require_relative 'boot'
 
-require 'rails/all'
+
+
+require "action_controller/railtie"
+require "active_model/railtie"
+require "action_mailer/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module Moeverdose
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
