0bfab423aeb28cdc3768b4db2145a92b4afb96bd
Aruisehu <Yohann.JOLAIN@supinfo.com>
2016-08-11 08:22:44 +0200

    Added type (indirectly) on tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,4 +6,20 @@ class Tag
 
   field :posts_count, type: Integer, default: 0
   has_and_belongs_to_many :posts, class_name: "Post", inverse_of: :tags
+
+  #Type of post we exclude possibility to have multiple type
+  has_one :author, class_name: "Author"
+  validates :author, presence: true, uniqueness: true, if: ["character.nil?", " content.nil?"]
+  validates :author, absence: true, if: "!character.nil?"
+  validates :author, absence: true, if: "!content.nil?"
+
+  field :character, type: String
+  validates :character, presence: true, uniqueness: true, if: ["author.nil?", " content.nil?"]
+  validates :character, absence: true, if: "!author.nil?"
+  validates :character, absence: true, if: "!content.nil?"
+
+  field :content, type: String
+  validates :content, presence: true, uniqueness: true, if: ["character.nil?", " author.nil?"]
+  validates :content, absence: true, if: "!author.nil?"
+  validates :content, absence: true, if: "!character.nil?"
 end
