8d2b3f0be3d849f4956a1f956d252901d504e8cc
Aruisehu <Yohann.JOLAIN@supinfo.com>
2016-08-13 22:46:57 +0200

    Changed tag type

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -7,6 +7,20 @@ class Tag
   field :posts_count, type: Integer, default: 0
   has_and_belongs_to_many :posts, class_name: "Post", inverse_of: :tags
 
-  field :type, type: String
-  validates :character, presence: true
+  field :type, type: Symbol
+
+  module Type
+    def self.all
+      [:content, :character, :author]
+    end
+
+    self.all.each do |type|
+      define_method("#{type}?") do
+        self.type == type
+      end
+    end
+  end
+  include Tag::Type
+  validates :type, {in: Tag::Type.all}
+
 end
