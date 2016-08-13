520ec55ce0740a44b0451edf048e3603253c5674
Erfani <sebastien.erfani@reveauxlettres.fr>
2016-08-13 22:38:46 +0200

    Started tag upload

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,6 +18,14 @@ class Post
   field :overdose, type: Integer, default: 0
   field :moe_shortage, type: Integer, default: 0
 
+  belongs_to :user, class_name: "User", inverse_of: :posts
+
+  belongs_to :author, class_name: "Author", inverse_of: :posts
+
+  has_many :comments, class_name: "Comment", inverse_of: :post
+
+  has_and_belongs_to_many :tags, class_name: "Tag", inverse_of: :posts
+
   field :report, type: Boolean, default: false
   alias_method :report?, :report
   has_one :report_user, class_name: "User"
