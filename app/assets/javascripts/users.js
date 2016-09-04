cd048dff91cd077148b5617303e0510f1d15ccd8
Erfani <sebastien.erfani@reveauxlettres.fr>
2016-09-04 09:04:13 +0200

    fix avatar/banner js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,9 +1,8 @@
 Useredit = {
   init: function(root) {
     Useredit.root = root;
-    console.log(Useredit.root);
-    Useredit.root.find('#user_avatar').change(function(){Useredit.update_image(Useredit.root.find('#avatar_im'),this)});
-    Useredit.root.find('#user_banner').change(function(){Useredit.update_image(Useredit.root.find('#banner'),this)});
+    Useredit.root.find('#user_avatar').change(function(){Useredit.update_image(Useredit.root.find('#avatar_img'),this)});
+    Useredit.root.find('#user_banner').change(function(){Useredit.update_image(Useredit.root.find('#banner_img'),this)});
   },
   update_image: function(image,input) {
     if (input.files && input.files[0]) {
