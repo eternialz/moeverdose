c9c9db17ce75a60c277a9a781a9a072b82db692a
Erfani <sebastien.erfani@reveauxlettres.fr>
2016-09-07 01:04:03 +0200

    Added notifications system

---
+++ app/assets/javascripts/notifications.js
@@ -0,0 +1,31 @@
+Notification = {
+  init: function(root) {
+    Notification.root = root
+  },
+  add: function(title, message, type) {
+    type = type || "";
+    if (message != undefined) {
+      message = "<p>" + message + "<p>";
+    } else {
+      message = ""
+    };
+    var title = "<h5>" + title + "</h5>";
+    var id = $('.notification').length + 1;
+
+    var notification = $("<div id='" + id + "' class='notification " + type + "'><i class='fa fa-close'></i>" + title + message + "</div>");
+
+    Notification.root.append( $(notification) );
+
+    notification.find(".fa-close").click( function() {
+      notification.remove();
+    });
+
+    setTimeout(function() {
+      notification.remove();
+    }, 7000);
+  }
+};
+
+$(document).ready(function(){
+  Notification.init($('.notifications'));
+});
