299f343dc17d4fd209c7cfe00ffa3fa7aeabf324
Erfani <sebastien.erfani@reveauxlettres.fr>
2016-11-11 15:49:27 +0100

    Changed name string to names array for tag aliases

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,7 @@ class Tag
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :name, type: String
+  field :names, type: Array, default: []
 
   field :posts_count, type: Integer, default: 0
   has_and_belongs_to_many :posts, class_name: "Post", inverse_of: :tags
