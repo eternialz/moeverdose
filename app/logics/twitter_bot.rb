afde6b2f76f3f0aad20f1134cdfb825a41b113b9
Aruisehu <Yohann.JOLAIN@supinfo.com>
2017-07-27 09:54:41 +0200

    Modified query in TwitterBot.run to be more readable and clean

--- app/logics/twitter_bot.rb
+++ app/logics/twitter_bot.rb
@@ -1,6 +1,6 @@
 class TwitterBot
     def self.run
-        post = Post.where(report: false, :created_at.gt => DateTime.now - 1).order('overdose DESC').order('created_at DESC').first
+        post = Post.where(report: false, :created_at.gt => DateTime.now - 1).order(overdose: :desc, created_at: :desc).first
         url = Rails.application.routes.url_helpers.post_url(post.number)
         $client.update("Best image of the day: #{url}")
         
