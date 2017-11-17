d1cd438b999608f3c84c96ffd051c577e4f942a6
Aruisehu <Yohann.JOLAIN@supinfo.com>
2017-11-17 09:57:37 +0100

    If no post in the day, twitter bot do not post empty tweet

--- app/logics/twitter_bot.rb
+++ app/logics/twitter_bot.rb
@@ -1,9 +1,11 @@
-class TwitterBot
+module TwitterBot
     def self.run
-        post = Post.where(report: false, :created_at.gt => DateTime.now - 1).order(overdose: :desc, created_at: :desc).first
-        url = Rails.application.routes.url_helpers.post_url(post.number)
-        $client.update("Best image of the day: #{url}")
-        
+        if ENV["RAILS_ENV"] == "production"
+            post = Post.where(report: false, :created_at.gt => DateTime.now - 1).order(overdose: :desc, created_at: :desc).first
+            if post
+                url = Rails.application.routes.url_helpers.post_url(post.number)
+                $client.update("Best image of the day: #{url}")
+            end
+        end
     end
-
 end
