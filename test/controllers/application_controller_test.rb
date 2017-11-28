172bb8e79f54cff76a5c9659835180a6ba063dfa
Sébastien Erfani <sebastien.erfani@gmail.com>
2017-11-28 11:41:03 +0100

    Added application controller test for home page

---
+++ test/controllers/application_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class ApplicationControllerTest < ActionDispatch::IntegrationTest
+    test 'Home Page' do
+        get root_path
+        assert_response :success
+    end
+end
