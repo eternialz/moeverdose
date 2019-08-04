0ff9d69d3b9bbf5f6608ccaa005c2549c52fa142
Sébastien Erfani <sebastien.erfani@supinfo.com>
2019-08-04 10:03:23 +0200

    Fix test

--- test/services/post_service_test.rb
+++ test/services/post_service_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 class UserServiceTest < ActiveSupport::TestCase
 
     setup do
-        FactoryBot.rewind_sequences
         @last_post = create(:user_with_post).posts.first
     end
 
--- test/services/tag_service_test.rb
+++ test/services/tag_service_test.rb
@@ -1,17 +1,16 @@
 require 'test_helper'
 
 class TagServiceTest < ActiveSupport::TestCase
-
     setup do
     end
 
     test 'tag_sanitization' do
-        assert_equal "abc", TagService.sanitize("_abc")
-        assert_equal "abc", TagService.sanitize("abc_")
-        assert_equal "abc", TagService.sanitize("_abc_")
-        assert_equal "abc", TagService.sanitize("AbC")
-        assert_equal "abc", TagService.sanitize("a*b~c")
-        assert_equal "ab_c", TagService.sanitize("ab c")
-        assert_equal "a_b_c", TagService.sanitize("a\nb    \nc   ")
+        assert_equal 'abc', TagService.sanitize('_abc')
+        assert_equal 'abc', TagService.sanitize('abc_')
+        assert_equal 'abc', TagService.sanitize('_abc_')
+        assert_equal 'abc', TagService.sanitize('AbC')
+        assert_equal 'abc', TagService.sanitize('a*b~c')
+        assert_equal 'ab_c', TagService.sanitize('ab c')
+        assert_equal 'a_b_c', TagService.sanitize('a\nb    \nc   ')
     end
 end
