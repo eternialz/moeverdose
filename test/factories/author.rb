bde715928b58141d30c0be0f8da7bf4ad9cbcb74
Sébastien Erfani <213969@supinfo.com>
2019-08-25 18:40:05 +0200

    Fix #45, add test for user service level increase

--- test/factories/author.rb
+++ test/factories/author.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
     factory :author do
         name { TagService.sanitize(Faker::TvShows::FamilyGuy.character) }
         biography { Faker::TvShows::FamilyGuy.quote }
-        association :tag, factory: :tag_author
+        tag { Tag.new(type: :author, aliases: [Alias.new(main: true, name: TagService.sanitize(name).to_s)]) }
         website { Faker::Internet.url }
     end
 end
--- test/services/user_service_test.rb
+++ test/services/user_service_test.rb
@@ -5,7 +5,8 @@ class UserServiceTest < ActiveSupport::TestCase
         FactoryBot.rewind_sequences
         Level.destroy_all
         @user = create(:user)
-        create(:level_final, rank: 1)
+        create(:level, rank: 1)
+        create(:level_final, rank: 2)
         @user.exp = 0
         @user.upload_count = 0
         @user.save
@@ -17,6 +18,16 @@ class UserServiceTest < ActiveSupport::TestCase
         assert_equal 1, @user.upload_count
         assert_equal 1, @user.exp
         assert_equal 0, @user.level.rank
+        assert @user.persisted?
+    end
+
+    test 'update_level_with_level_increase' do
+        @user.exp = @user.level.max_exp
+
+        assert_difference -> { @user.level.rank }, 1 do
+            UserService.update_level(@user)
+        end
+
         assert @user.persisted?
     end
 end
