fa3c0ec1c6a3dbc2120af9251eb812998e672a77
Sébastien Erfani <sebastien.erfani@supinfo.com>
2019-09-07 12:27:03 +0200

    Change file watcher to support hot reload in docker

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
     # Use an evented file watcher to asynchronously detect changes in source code,
     # routes, locales, etc. This feature depends on the listen gem.
-    config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+    config.file_watcher = ActiveSupport::FileUpdateChecker
 
     # Open mails using letter_opener in dev instead of trying to sending them and erroring
     config.action_mailer.default_url_options = { host: 'localhost:3000' }
